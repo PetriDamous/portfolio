5765eca42d4750444a7cfdeb5165368e5f859847
dwilliams64 <dwilliams_64@yahoo.com>
2021-09-06 11:48:23 -0500

    installed image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
